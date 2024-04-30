aa887c3c97ae4437583549b183db670a0d410c23
Jeffrey Dwyer <jeffreydwyer95@outlook.com>
2024-04-30 18:52:06 +0000

    Updates config to use postges and nginx proxy

--- nextcloud.nix
+++ nextcloud.nix
@@ -1,17 +1,63 @@
 { config, pkgs, ...}:
 
-{
+let 
+  hostName = "www.wizardspacegiraffe.com";
+in {
+
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimization = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts = {
+      "${hostName}" = {
+        forceSSL = true;
+        enableACME = true;
+      };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "prorate.theater286@4wrd.cc" ;
+  };
+
+  environment.etc."nextcloud-secrets.json".source = ./secrets/nextcloud-secrets.json;
+  environment.etc."nextcloud-admin-pass".source = ./secrets/nextcloud-admin-pass;
+
   services.nextcloud = {
     enable = true;
-    hostName = "localhost";
+    inherit hostName;
+    https = true;
     package = pkgs.nextcloud28;
     extraApps = {
       inherit (config.services.nextcloud.package.packages.apps);
     };
     extraAppsEnable = true;
-    config.adminpassFile = "/etc/nextcloud-admin-pass";
+    config = {
+      dbtype = "pgsql";
+      dbuser = "nextcloud";
+      dbhost = "/run/postgresql";
+      adminuser = "root";
+      adminpassFile = "/etc/nextcloud-admin-pass";
+    };
     secretFile = "/etc/nextcloud-secrets.json";
   };
-  environment.etc."nextcloud-secrets.json".source = ./secrets/nextcloud-secrets.json;
-  environment.etc."nextcloud-admin-pass".source = ./secrets/nextcloud-admin-pass;
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "nextcloud" ];
+    ensureUsers = [
+      {
+        name = "nextcloud";
+        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
+      }
+    ];
+  }; 
+
+  systemd.services."nextcloud-setup" = {
+    requires = [ "postgresql.service" ];
+    after = [ "postgresql.service" ];
+  };
+
 }
