14b1ef1c03268b48a9a3383a8b00572bfd6feeb0
Jeffrey Dwyer <jeffreydwyer95@outlook.com>
2024-04-30 14:00:10 +0000

    Removes DHCP config

--- networking.nix
+++ networking.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }: 
 
 {
-  
-  networking.useDHCP = false;
-  networking.interfaces.enp0s3.useDHCP = true;
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 80 443 ]; 
